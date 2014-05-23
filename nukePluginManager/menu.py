2ddc4b2104881dfe034d4761a4cbb1783e45743e
Simon Jokuschies <simon.jokuschies@gmx.de>
2014-05-23 12:18:09 +0200

    ..

--- nukePluginManager/menu.py
+++ nukePluginManager/menu.py
@@ -2,10 +2,6 @@ import nuke
 import nukePluginManager
 import nukescripts
 
-#nuke.menu("Nuke").addCommand('Scripts/NukePluginManager', 'nukePluginManager.runPluginManager()')
-
-
-
 nuke.menu('Pane').addCommand('Nuke Plugin Manager', nukePluginManager.addPMPanel)
 nukescripts.registerPanel('com.ohufx.pluginManager', nukePluginManager.addPMPanel)
 

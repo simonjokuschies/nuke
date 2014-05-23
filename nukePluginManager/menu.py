df31d978c6539034334ef313d801549574b9aa8d
Simon Jokuschies <simon.jokuschies@gmx.de>
2014-05-23 12:17:13 +0200

    update plugin manager

--- nukePluginManager/menu.py
+++ nukePluginManager/menu.py
@@ -2,6 +2,12 @@ import nuke
 import nukePluginManager
 import nukescripts
 
+#nuke.menu("Nuke").addCommand('Scripts/NukePluginManager', 'nukePluginManager.runPluginManager()')
+
+
+
 nuke.menu('Pane').addCommand('Nuke Plugin Manager', nukePluginManager.addPMPanel)
 nukescripts.registerPanel('com.ohufx.pluginManager', nukePluginManager.addPMPanel)
+
+nukePluginManager.init()
 
